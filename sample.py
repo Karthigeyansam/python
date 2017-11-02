25f2e8c0cef4978d26a174129b1d478f0aff4cb5
karthigeyan19 <karthigeyan19@gmail.com>
2017-11-02 04:43:42 -0400

    test

--- sample.py
+++ sample.py
@@ -1,1 +1,14 @@
-print "Hello World"
+class test:
+  "This is test"
+  def __init__(self, i=0, j=0):
+    self.i = i
+    self.j = j
+    print(j)
+  def getdata(self):
+    print(self.i)
+
+ob1 = test(1,2)
+#ob1.attr = 2
+ob1.getdata()
+
+
